a29b8b7b0f49985c08bd78143d96bcfd113c24cc
Slava <nlo_tomsk@mail.ru>
2023-04-24 20:59:07 +0700

    init

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,9 @@
-// TODO: write your code here
-import sum from './basic';
-
-console.log('worked');
-
-console.log(sum([1, 2]));
+export default function healthIndicator(characterData) {
+    if (characterData.health > 50) {
+      return 'healthy';
+    }
+    if (characterData.health >= 15) {
+      return 'wounded';
+    }
+    return 'critical';
+  }
